9fe12e5022921d6ec8aad6905961d767884bdf8a
Sasha <alecsand.unit@gmail.com>
2024-12-16 01:14:07 +0200

    dz

--- Task2/ContactL.cs
+++ Task2/ContactL.cs
@@ -8,7 +8,7 @@ namespace Task2
 {
     public class ContactL
     {
-        public string FullName { get; set; }
+        public string FullName { get; set; } 
         public string Address { get; set; }
         public string Phone { get; set; }
     }
